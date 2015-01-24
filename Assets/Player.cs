e5d9b6c61a19133d64c69481fba4a696c125405c
gmfnasg12 <gpx_mark_farng@hotamail.com>
2015-01-24 12:35:55 +0800

    addInput

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,18 +5,21 @@ using GoingUp;
 namespace GoingUp
 {
 public class Player : Actor {
+	KeyCode keyType = KeyCode.Space;
+	private	static float currentlyHp = 0;
+	private static float originalHp = 100;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
+		private float hp_;
+		private float o2value_;
+		public  float o2LostSpeed;
+
+		public float HP { get { return hp_; } set { hp_ = value; } }
+		public float O2Value { get { return o2value_; } set { o2value_ = value; } }
+
 	void Update () {
 
 			if (isBreathing ()) 
 			{
-
 						
 			}
 
@@ -25,25 +28,25 @@ public class Player : Actor {
 	}
 
 
-		public bool isBreathing()
-		{
-			return true;
+	public bool isBreathing()
+	{
+		bool onBreating = false;
+		//get input
+		if (Input.touchCount > 0 || Input.GetKey (keyType)) {
+			onBreating = true;
+			Debug.Log ("Info Breathing(" + onBreating+")");
 		}
+		return onBreating;
+	}
+
 
 	public void effect( NPC npc )
 	{
 
+
 	}
 
-	private float hp_;
-	private float o2value_;
-	public  float o2LostSpeed;
-	
 
-
-
-	public float HP { get { return hp_; } set { hp_ = value; } }
-	public float O2Value { get { return o2value_; } set { o2value_ = value; } }
 }
 
 }
