07fc312b9a4e477c71a7ec65c51d7766ce2bab65
ayo1103 <ayo1103@gmail.com>
2015-01-25 11:38:56 +0800

    Add end panel slide up animation

--- Assets/Scripts/GameOverBoard.cs
+++ Assets/Scripts/GameOverBoard.cs
@@ -4,9 +4,35 @@ using System.Collections;
 
 public class GameOverBoard : MonoBehaviour 
 {
+//	public float upSpeed = 10.0f;
 	public Image panel;
 	public Text scoreText;
 	public Text bestText;
-
-
+//
+//	public void Start()
+//	{
+////		SlideUp();
+//	}
+//
+//	public void Update()
+//	{
+//		if (panel.rectTransform.position.y > 0)
+//		{
+//			panel.rectTransform.Translate(Vector3.up * Time.deltaTime * upSpeed);
+//		}
+//	}
+//
+//	public void SlideUp()
+//	{
+//		StartCoroutine(PanelMoveUp());
+//	}
+//
+//	IEnumerator PanelMoveUp()
+//	{
+//		while (panel.transform.position.y > 0)
+//		{
+//			yield return null;
+//			panel.transform.Translate(Vector3.up * Time.deltaTime * upSpeed);
+//		}
+//	}
 }
